3cad28139d2cc116b1800396838e88a68f1d483c
Amappola <anam.porrasp@gmail.com>
2023-12-13 05:24:10 -0500

    Advancing in some tests

--- src/app/ui/pages/calculator/calculator.component.spec.ts
+++ src/app/ui/pages/calculator/calculator.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CalculatorComponent } from './calculator.component';
+import { ResultScreenComponent } from '../../shared/result-screen/result-screen.component';
+import { OperationButtonComponent } from '../../shared/operation-button/operation-button.component';
+import { NumericButtonComponent } from '../../shared/numeric-button/numeric-button.component';
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
@@ -8,7 +10,7 @@ describe('CalculatorComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [CalculatorComponent]
+      declarations: [CalculatorComponent, ResultScreenComponent, OperationButtonComponent, NumericButtonComponent]
     });
     fixture = TestBed.createComponent(CalculatorComponent);
     component = fixture.componentInstance;
